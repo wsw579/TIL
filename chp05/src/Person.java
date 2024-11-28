982a5a3e0c4d2371d7ad1d6f13e5c7b94df1bc36
wsw <rmfltmsaktls@naver.com>
2024-11-28 12:33:08 +0900

    Changes

--- chp05/src/Person.java
+++ chp05/src/Person.java
@@ -1,16 +1,16 @@
 public class Person {
     private String name;
 
-    public Person(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String newName) {
         this.name = newName;
+    }
+
+    public Person(String name) {
+        this.name = name;
     }
 
     public static void main(String[] args) {
