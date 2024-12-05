d15d9945b6e75e869109f0e4420dcf969adfbc34
victoryone <65767043+wsw579@users.noreply.github.com>
2024-12-05 16:44:26 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -53,6 +53,9 @@ for entry in feed.entries:
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(content)
 
+    
+    print(f"Adding file to git: {file_path}")
+
     # 깃허브 커밋
     repo.git.add(file_path)
     repo.git.commit('-m', f'Add post: {entry.title}')
